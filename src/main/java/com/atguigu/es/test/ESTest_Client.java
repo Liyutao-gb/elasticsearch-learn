73389760a3bca393c6c331790f93e3a925fe0b48
liyutao-gb <66844549@qq.com>
2021-08-03 07:57:52 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/es/test/ESTest_Client.java
@@ -0,0 +1,25 @@
+package com.atguigu.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+/**
+ * @author liyutao
+ * @since 2021/8/3 0:45
+ */
+public class ESTest_Client {
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        client.close();
+
+
+
+    }
+
+}
